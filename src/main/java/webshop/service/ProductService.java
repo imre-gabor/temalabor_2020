7601065dfaebc6439560fcee8e804b97ad624961
imre-gabor <gabor@aut.bme.hu>
2020-11-02 16:04:04 +0100

    ProductService springes annotációkkal

--- src/main/java/webshop/service/ProductService.java
+++ src/main/java/webshop/service/ProductService.java
@@ -1,13 +1,23 @@
 package webshop.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import webshop.repository.CategoryRepository;
 import webshop.repository.ProductRepository;
 
+@Service
 public class ProductService {
 
+    @Autowired
     CategoryRepository categoryRepository;
+    
+    @Autowired
     ProductRepository productRepository;
     
+    
+    @Transactional
     public void discountProductsByCategoryName(String catName, int percent) {
         
         categoryRepository.findByName(catName).stream().forEach(c ->{

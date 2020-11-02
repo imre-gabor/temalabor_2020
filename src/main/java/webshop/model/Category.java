54264a3df641ca9e2b84d8c299d384ee47ff30de
imre-gabor <gabor@aut.bme.hu>
2020-11-02 16:03:21 +0100

    Category és Product átalakítás JPA entitássá

--- src/main/java/webshop/model/Category.java
+++ src/main/java/webshop/model/Category.java
@@ -1,16 +1,41 @@
 package webshop.model;
 
+import java.util.ArrayList;
 import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Category {
-    
+
+    @Id
+    @GeneratedValue
     private long id;
+
     private String name;
 
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    @OneToMany(mappedBy = "category")
     private Collection<Product> products;
+
+    public Collection<Product> getProducts() {
+        if (this.products == null)
+            this.products = new ArrayList<>();
+
+        return this.products;
+    }
 }
--- src/main/java/webshop/model/Product.java
+++ src/main/java/webshop/model/Product.java
@@ -1,14 +1,36 @@
 package webshop.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product {
 
+    @Id
+    @GeneratedValue
     private long id;
+    
     private String name;
     private int price;
+    
+    public Product() {
+    }
+    
+    public Product(String name, int price) {
+        super();
+        this.name = name;
+        this.price = price;
+    }
+
+
+
+    @ManyToOne
     private Category category;
 }
